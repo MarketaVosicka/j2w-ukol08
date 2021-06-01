ec011546bdcc5450662e865c5074fa46f3da91d5
Markéta Vošická <vosickamarketa@gmail.com>
2021-06-01 21:34:41 +0200

    přidán V2__data.sql

---
+++ src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,55 @@
+INSERT INTO post (slug, author, title, perex, body, published)
+VALUES ('neuveritelne-rekordy-1', 'Markéta', 'Neuvěřitelné rekordy #1', 'Celých 21 minut a 33 sekund vydržel Švýcar Peter Colat v roce 2011 pod vodou se zadrženým dechem.', '<li>Celých 21 minut a 33 sekund vydržel Švýcar Peter Colat v roce 2011 pod vodou se zadrženým dechem.</li>
+<li>Britka Maureen Westonová zvládla v roce 1977 nespat v kuse 18 dnů a 17 hodin.</li>
+<li>Mozek člověka má přibližně kapacitu 20 000 průměrně velkých slovníků.</li>
+<li>150 °C snese lidský organismus krátkodobě v suchém vzduchu.</li>
+<li>170km/hod. dosahuje rychlost vzduchu při kýchání.</li>
+<li>Netrénovaný člověk rozeznává asi 4 000 pachů, trénovaný až 10 000.</li>
+<li>1 350 kg je zatížení, které snese holenní kost.</li>
+<br>
+<br>
+<em>zdroj: <a href="https://www.national-geographic.cz/clanky/rekordy-lidskeho-tela-neuveritelna-nej-obycejnych-smrtelniku-20160524.html">https://www.national-geographic.cz/clanky/rekordy-lidskeho-tela-neuveritelna-nej-obycejnych-smrtelniku-20160524.html</a></em>', '2019-03-01'),
+       ('neuveritelne-rekordy-2', 'Markéta', 'Neuvěřitelné rekordy #2', 'Nedobrovolnou rekordmankou s nejnižší zaznamenanou teplotou těla, která podchlazení přežila, je Švédka Anna Johansson Bågenholmová – v roce 1999 byla uvězněna 80 minut ve vzduchové kapse ve vodě pod ledem. Její tělesná teplota klesla na 13,7 °C.', '<li>Nedobrovolnou rekordmankou s nejnižší zaznamenanou teplotou těla, která podchlazení přežila, je Švédka Anna Johansson Bågenholmová – v roce 1999 byla uvězněna 80 minut ve vzduchové kapse ve vodě pod ledem. Její tělesná teplota klesla na 13,7 °C.</li>
+<li>Zdravý člověk s dostatečným přísunem tekutin dokáže bez jídla přežít okolo 50 dní. V suchu a teple může být bez vody 7 až 10 dní. Nejdéle přežil bez potravy Američan Charles Robert McNabb, který v roce 2004 ve vězení hladověl 123 dní. Pil pouze vodu a občas trochu kávy. </li>
+<li>Rakušan Felix Baumgartner loni volným pádem překonal rychlost zvuku. Rychlosti přes 1 357,64 km/hod. dosáhl 40 vteřin po seskoku z balonu ve výšce 39 km.</li>
+<li>Brit Gary Connery loni ve speciální kombinéze skočil bez padáku z výšky asi 730m a bez zranění dopadl do hromady připravených krabic. Během letu zrychlil na téměř 130 km/hod</li>', '2019-03-15'),
+       ('neuveritelne-rekordy-3', 'Markéta', 'Neuvěřitelné rekordy #3', 'Nejvyššího věku údajně dosáhl Dán Christian Jacobsen Drackenberg, jenž se narodil v roce 1626 a zemřel v necelých 146 letech.', '<li>Nejvyššího věku údajně dosáhl Dán Christian Jacobsen Drackenberg, jenž se narodil v roce 1626 a zemřel v necelých 146 letech.</li>
+<li>Za nejstarší ženu světa je považována Francouzka Jeanne Louise Calmentová (1875–1997). Dožila se 122 let a 164 dní.</li>
+<li>Nejstarší matce, Indce Omkari Panwarové, bylo 70 let, když v roce 2008 přivedla na svět díky umělému oplodnění dvojčata.</li>
+<li>Nejstaršímu otci, Indovi Ramajitu Raghavovi, bylo 96 let, když mu loni v říjnu jeho 52letá žena porodila chlapečka.</li>
+<li>Nejmladší matce, Peruánce Lině Medina, bylo 5 let, 7 měsíců a 21 dní, když v květnu 1939 porodila zdravého chlapečka. Otcem byl zřejmě její vlastní otec.</li>
+<li>Valentina Vassiljevová, žena ruského rolníka, přivedla mezi lety 1725–1765 na svět 69 dětí (16 x porodila dvojčata, 7x trojčata a 4x čtyřčata).</li>
+<li>Největší doložený počet dětí narozených při jednom porodu je 10 (1946, Brazílie).</li>
+<li>Američanka Nadya Sulemanová porodila v roce 2009 díky umělému oplodnění 8 dětí. „Octomom" (matka osmerčat) přitom už měla doma šest ratolestí. I ty počala po umělém oplodnění.</li>
+<li>Australan Allan Stewart se stal loni v květnu nejstarším diplomovaným studentem světa. V 97 letech obdržel už svůj čtvrtý univerzitní diplom, tentokrát magistra lékařských věd.</li>', '2019-04-02'),
+       ('zkouska', 'Markéta', 'Zkouška', 'Zkouška :)', '90-60-90. Funguje to !!', '2019-07-02'),
+       ('lorem-ipsum', 'Markéta', 'Lorem ipsum', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In dapibus augue non sapien. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Aenean vel massa quis mauris vehicula lacinia. Nullam dapibus fermentum ipsum. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Quisque tincidunt scelerisque libero. Aenean fermentum risus id tortor.', '2020-03-30'),
+       ('hello-world', 'Markéta', 'Hello World', 'Hello World', 'Hello World, Ahoj světe', '2018-01-01'),
+       ('vtip-1', 'Markéta', 'Taxík', 'Do taxíku nastoupí zcela nahá dívka', 'Do taxíku nastoupí zcela nahá dívka a jakoby nic oznámí, kam chce odvézt. Řidič se cestou neustále ohlíží. „Neviděl jste nikdy nahou ženskou?" zlobí se dívka. „Ale jo. Jen mě zajímá, odkud při placení vytáhnete peněženku..." poznamená taxíkář', '2020-05-02'),
+       ('vtip-2', 'Markéta', 'Bulharsko', 'Pan Novák navštívil s rodinou Bulharsko.', 'Pan Novák navštívil s rodinou Bulharsko. Na jednom zvláště hezkém místě vyryl do skály: „Byl jsem zde se svou ženou, je zde krásně." Příští rok tam přijela paní Nováková sama. Do skály pod původní nápis připsala: „Letos je tu mnohem krásněji', '2020-05-05'),
+       ('vtip-3', 'Markéta', 'Matterhorn', 'V Alpách přijde turista za horským vůdcem, aby ho zavedl na vrchol Matterhornu.', 'V Alpách přijde turista za horským vůdcem, aby ho zavedl na vrchol Matterhornu. „Nezlobte se, ale já už to několik roků nedělám." „A proč?" „Špatně jsem snášel tu samotu při sestupu.', '2020-05-11'),
+       ('vtip-4', 'Markéta', 'Prázdniny', '„Tak jste se dověděli, že se těleso při zahřívání roztahuje a při ochlazení stahuje.', '„Tak jste se dověděli, že se těleso při zahřívání roztahuje a při ochlazení stahuje. Uveďte nějaký příklad." Hlásí se Borisek: „Prosím, prázdniny. Letní jsou dva měsíce a zimní jenom týden.', '2020-05-22'),
+       ('vtip-5', 'Markéta', 'Inzerát', '„Já jdu na ten inzerát, že dáte pejska do dobrých rukou..."', 'Zazvoní divný chlap.
+„Já jdu na ten inzerát, že dáte pejska do dobrých rukou..." říká.
+„Jdete pozdě, už tu byl jiný před vámi," slyší odpověď.
+„Aha... a něco jiného k snědku byste neměli?"', '2020-06-22'),
+       ('vtip-6', 'Markéta', 'Žížaly', 'Potkaly se dvě žížaly.', 'Potkaly se dvě žížaly.
+„Kde máš starýho?" ptá se jedna.
+„Včera ho vytáhli na ryby...', '2020-07-05'),
+       ('vtip-7', 'Markéta', 'Mravenec a slon', 'Mravenec se slonem se vypravili na výlet.', 'Mravenec se slonem se vypravili na výlet. Šli cestou necestou, lesem, pralesem i pouští, přes hory, řeky, rybníky. A kde se vzali, tu se vzali, kráčeli najednou na dřevěné lávce. Chvíli šli úplně mlčky, ale po několika okamžicích mravenec zálibně pravil: „Slone, slyšíš?" „A co?" tázal se slon. „Jak pěkně dupeme."', '2020-08-01'),
+       ('vtip-8', 'Markéta', 'Beruška', 'Přijde beruška na karneval a tváře má nacpané tvarohem.', 'Přijde beruška na karneval a tváře má nacpané tvarohem. Přijde k ní šneček a ptá se: "Za co jdeš?" Beruška si zmáčkne tváře a odpoví:,,Za beďara."', '2020-08-12'),
+       ('vtip-9', 'Markéta', 'Papoušek', 'Paní si jde koupit papouška.', 'Paní si jde koupit papouška. Vybere si takového malého, a zeptá se ho:"Tak co, ty malej parchante, umíš mluvit?" A on říká:"A co ty stará čarodějnice, umíš lítat?"', '2020-08-29'),
+       ('vtip-10', 'Markéta', 'Velbloud', 'Ptá se slon velblouda: „Proč máš prsa na hřbetě?', 'Ptá se slon velblouda: „Proč máš prsa na hřbetě?”
+„Hmm,” odpovídá velbloud, „zajímavá otázka od někoho, komu visí penis s ksichtu.”', '2020-10-12'),
+       ('vtip-11', 'Markéta', 'Žralok', 'Která tři slova slyší žralok nejraději, když pluje za lodí?', 'Která tři slova slyší žralok nejraději, když pluje za lodí?
+Muž přes palubu!', '2020-11-02'),
+       ('vtip-12', 'Markéta', 'Bůh', 'Pes: „Pán mě krmí, stará se o mě, dává mi střechu nad hlavou', 'Pes: „Pán mě krmí, stará se o mě, dává mi střechu nad hlavou - určitě to je Bůh!”
+Kočka: „Pán mě krmí, stará se o mě, dává mi střechu nad hlavou - určitě jsem Bůh!”', '2020-11-11'),
+       ('vtip-13', 'Markéta', 'Medvěd', 'Po lese chodí strašně naštvaný medvěd.', 'Po lese chodí strašně naštvaný medvěd. Všude okolo sníh, mrzne až praští. Medvěd tu kopne do jedličky, tam do zajíce, vlkovi rozbije hubu a při tom si mrmlá: „Proč jsem kurva v září pil to kafe...?!”', '2020-12-30'),
+       ('vtip-14', 'Markéta', 'Zajíc', 'Jde zajíc po poušti, potká ho žirafa a ptá se:', 'Jde zajíc po poušti, potká ho žirafa a ptá se: "Zajíci, proč za sebou táhneš ty dveře od auta?"
+A zajíc na to: "Kdyby mi bylo horko, tak si otevřu okýnko."', '2021-02-06'),
+       ('vtip-15', 'Markéta', 'Zlatá ryba', 'Dědeček vyloví zlatou rybku. ', 'Dědeček vyloví zlatou rybku. Rybka řekne dědečkovi:"když mě pustíš splnímti tři přání". Dédeček rybce odpoví:"toti nepomůže,já jsem kouzelný dědeček.', '2021-04-19');
+
+
+
+
